a0760c8c85dab5fc131d971d5e763868f6ffdccc
Andrew Barger <asbarg01@gmail.com>
2024-07-14 00:22:59 -0400

    Initialize basic tf configuration

---
+++ src/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  # Sensitive backend configuration will be provided as file or environment variables. See https://developer.hashicorp.com/terraform/language/settings/backends/configuration#file
+  backend "azurerm" {
+    storage_account_name = "saprimaryvnettf1"
+    container_name       = "tfstate-container"
+    key                  = "terraform.tfstate"
+    #use_oidc signals we will use an SPN with Workload Identity Federation
+    use_oidc             = true
+    #use_azuread_auth signals the storage account uses Azure AD for authentication rather than an access key
+    use_azuread_auth     = true
+  }
+}
---
+++ src/providers.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+
+  # We assume that the principal used to apply is an 
+  # Azure service principal using workload identity federation
+  # through Azure DevOps.
+  use_oidc = true
+
+  # Instruct Terraform to use Azure AD rather than SAS keys for storage blob and queue operations
+  storage_use_azuread = true
+}
+
+provider "azuread" {
+  use_oidc = true
+  tenant_id = var.azure_tenant_id
+}
